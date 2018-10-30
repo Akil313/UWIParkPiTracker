386938cdb6fb7f039554877f5550a043cb2b0675
Akil313 <akilhosang@gmail.com>
2018-10-30 04:58:00 -0400

    Add initial files

---
+++ raspPi.py
@@ -0,0 +1,63 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import time
+
+class ParkingLot:
+
+    def __init__(self, lot):
+
+        self.lot = lot
+        self.lotInfo = lot.to_dict()
+
+        self.name, self.pSpaces, self.tSpaces, self.location = self.extractInfo()
+
+    def extractInfo(self):
+        print(self.lotInfo)
+
+        name = self.lot.id
+        pSpaces = self.lotInfo['parking_spaces']
+        tSpaces = self.lotInfo['taken_spaces']
+        location = self.lotInfo['location']
+
+        return name, pSpaces, tSpaces, location
+
+    def setTSpaces(self, num):
+
+        if type(num) == int:
+
+            self.tSpaces = num
+
+
+def activate():
+
+    while True:
+
+        time.sleep(10)
+        print("Sending new info")
+        parkingLots[0].setTSpaces(parkingLots[0].tSpaces + 5)
+        print("Available spaces:", parkingLots[0].tSpaces)
+
+        time.sleep(10)
+        print("Sending new info")
+        parkingLots[0].setTSpaces(parkingLots[0].tSpaces - 5)
+        print("Available spaces:", parkingLots[0].tSpaces)
+
+
+
+cred = credentials.Certificate('UWIParkServiceAccountKey.json')
+default_app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+uwi = db.collection(u'parking_lots')
+lots = uwi.get()
+
+x = 0
+parkingLots = []
+
+for lot in lots:
+    parkingLots.append(ParkingLot(lot))
+
+    x = x + 1
+
+activate()
---
+++ raspPiConda.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 29 22:48:14 2018
+
+@author: Akil313
+"""
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
